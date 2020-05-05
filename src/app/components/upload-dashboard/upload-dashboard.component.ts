d7e7f053b22b835ec79994524083b389ce0a6bfd
Taha Sadraoui <taha.sadraoui@tunix.io>
2020-05-05 22:13:37 +0100

    Add Apply

--- src/app/components/upload-dashboard/upload-dashboard.component.ts
+++ src/app/components/upload-dashboard/upload-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as $ from "jquery";
+
 @Component({
   selector: 'app-upload-dashboard',
   templateUrl: './upload-dashboard.component.html',
@@ -10,6 +12,22 @@ export class UploadDashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(document).ready(function(){
+      $(".files").attr('data-before',"Drag file here or click the above button");
+      $('input[type="file"]').change(function(e){
+      let file = (<HTMLInputElement>e.target).files[0];
+      var fileName = file.name;
+      $(".files").attr('data-before',fileName);
+      
+      });
+      });
+  }
+
+  onApply():void{
+    console.log("Apply success");
+    
+
   }
 
 }
